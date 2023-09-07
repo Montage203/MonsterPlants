d0d25015fe33e8c735cf98492a48f205f890db3d
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-09-07 03:54:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,35 @@
-// Variables pour suivre l'état du jeu
-let seeds = 0;
-let plants = [];
+// script.js
+
+let level = 1;
 let credits = 0;
+let upgradeCost = 10;
 
-// Fonction pour acheter une graine de niveau 1
-document.getElementById("buy-seed").addEventListener("click", function () {
-    seeds++;
+const levelElement = document.getElementById("level");
+const creditsElement = document.getElementById("credits");
+const plantButton = document.getElementById("plant-button");
+const upgradeButton = document.getElementById("upgrade-button");
+
+plantButton.addEventListener("click", function () {
+    credits += level;
     updateGameUI();
 });
 
-// Fonction pour planter une graine
-document.getElementById("plant-container").addEventListener("click", function (event) {
-    const target = event.target;
-    if (target.classList.contains("plant")) {
-        const plantIndex = target.getAttribute("data-index");
-        if (seeds > 0 && !plants[plantIndex].planted) {
-            seeds--;
-            plants[plantIndex].planted = true;
-            updateGameUI();
-            setTimeout(growPlant, 30 * 60 * 60 * 1000, plantIndex); // 30 heures en millisecondes
-        }
-    }
-});
-
-// Fonction pour faire pousser une plante
-function growPlant(plantIndex) {
-    plants[plantIndex].planted = false;
-    credits += plants[plantIndex].creditReward;
-    updateGameUI();
-}
-
-// Fonction pour évoluer une plante avec la potion magique
-document.getElementById("credits").addEventListener("click", function () {
-    if (credits >= 20) {
-        credits -= 20;
-        seeds += 4;
+upgradeButton.addEventListener("click", function () {
+    if (credits >= upgradeCost) {
+        level++;
+        credits -= upgradeCost;
+        upgradeCost *= 2; // Coût d'amélioration doublé à chaque fois
         updateGameUI();
     }
 });
 
-// Fonction pour mettre à jour l'interface du jeu
 function updateGameUI() {
-    document.getElementById("seeds-count").textContent = seeds;
-    document.getElementById("credits-count").textContent = credits;
-
-    const plantContainer = document.getElementById("plant-container");
-    plantContainer.innerHTML = "";
-
-    for (let i = 0; i < plants.length; i++) {
-        const plant = plants[i];
-        const plantElement = document.createElement("div");
-        plantElement.classList.add("plant");
-        if (plant.planted) {
-            plantElement.classList.add("planted");
-        }
-        plantElement.setAttribute("data-index", i);
-        plantContainer.appendChild(plantElement);
-    }
+    levelElement.textContent = level;
+    creditsElement.textContent = credits;
+    upgradeButton.textContent = `Amélioration (${upgradeCost} crédits)`;
 }
 
-// Initialisation du jeu
-function initializeGame() {
-    for (let i = 0; i < 5; i++) {
-        plants.push({ planted: false, creditReward: 0 });
-    }
+// Mettre à jour l'interface au chargement de la page
+window.onload = function () {
     updateGameUI();
-}
-
-// Appel de la fonction d'initialisation du jeu au chargement de la page
-window.onload = initializeGame;
+};
