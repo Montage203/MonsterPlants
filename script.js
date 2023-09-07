a38f1732b118cdb2981f24e2a7b98129fac198f8
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-09-07 04:03:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 let level = 1;
 let credits = 0;
 let upgradeCost = 10;
+let resetCost = 100; // Coût de la réinitialisation au niveau 1
 
 const levelElement = document.getElementById("level");
 const creditsElement = document.getElementById("credits");
 const plantButton = document.getElementById("plant-button");
 const upgradeButton = document.getElementById("upgrade-button");
+const resetButton = document.getElementById("reset-button");
+const upgradeList = document.getElementById("upgrade-list");
 
 plantButton.addEventListener("click", function () {
     credits += level;
@@ -23,13 +26,33 @@ upgradeButton.addEventListener("click", function () {
     }
 });
 
-function updateGameUI() {
-    levelElement.textContent = level;
-    creditsElement.textContent = credits;
-    upgradeButton.textContent = `Amélioration (${upgradeCost} crédits)`;
+resetButton.addEventListener("click", function () {
+    if (level >= 7 && credits >= resetCost) {
+        credits -= resetCost;
+        level = 1;
+        resetCost *= 2; // Coût de réinitialisation doublé à chaque fois
+        // Ajoutez le bonus de réinitialisation ici
+        updateGameUI();
+    }
+});
+
+// Fonction pour ajouter une amélioration
+function addUpgrade(name, cost, increment) {
+    const upgradeItem = document.createElement("div");
+    upgradeItem.innerHTML = `<button>${name} (${cost} crédits)</button>`;
+    upgradeList.appendChild(upgradeItem);
+
+    upgradeItem.querySelector("button").addEventListener("click", function () {
+        if (credits >= cost) {
+            credits -= cost;
+            // Appliquez l'effet de l'amélioration ici (par exemple, augmentation automatique des crédits)
+            updateGameUI();
+        }
+    });
 }
 
 // Mettre à jour l'interface au chargement de la page
 window.onload = function () {
     updateGameUI();
+    // Ajoutez des améliorations ici (exemple: addUpgrade("Amélioration 1", 20, 1);)
 };
